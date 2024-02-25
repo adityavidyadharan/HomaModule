a0dbba55f981751483179c1f7e988b430c919ce5
Vidyadharan, Aditya Ram <vidyadharan@gatech.edu>
2024-02-25 18:17:14 -0500

    update kernel script

--- setup/kernel_patch.sh
+++ setup/kernel_patch.sh
@@ -2,10 +2,28 @@
 
 mkdir -p /tmp/kernel
 
-wget -P /tmp/kernel/ https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-headers-5.17.7-051707-generic_5.17.7-051707.202205121146_amd64.deb
-wget -P /tmp/kernel https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-headers-5.17.7-051707_5.17.7-051707.202205121146_all.deb
-wget -P /tmp/kernel https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-image-unsigned-5.17.7-051707-generic_5.17.7-051707.202205121146_amd64.deb
-wget -P /tmp/kernel https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-modules-5.17.7-051707-generic_5.17.7-051707.202205121146_amd64.deb
+# define urls list
+
+declare -a urls_5_17=(
+"https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-headers-5.17.7-051707-generic_5.17.7-051707.202205121146_amd64.deb"
+"https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-headers-5.17.7-051707_5.17.7-051707.202205121146_all.deb"
+"https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-image-unsigned-5.17.7-051707-generic_5.17.7-051707.202205121146_amd64.deb"
+"https://kernel.ubuntu.com/mainline/v5.17.7/amd64/linux-modules-5.17.7-051707-generic_5.17.7-051707.202205121146_amd64.deb"
+)
+
+declare -a urls_6_1=(
+    "https://kernel.ubuntu.com/mainline/v6.1.70/amd64/linux-headers-6.1.70-060170-generic_6.1.70-060170.202401011333_amd64.deb"
+    "https://kernel.ubuntu.com/mainline/v6.1.70/amd64/linux-headers-6.1.70-060170_6.1.70-060170.202401011333_all.deb"
+    "https://kernel.ubuntu.com/mainline/v6.1.70/amd64/linux-image-unsigned-6.1.70-060170-generic_6.1.70-060170.202401011333_amd64.deb"
+    "https://kernel.ubuntu.com/mainline/v6.1.70/amd64/linux-modules-6.1.70-060170-generic_6.1.70-060170.202401011333_amd64.deb"
+)
+
+urls=("${urls_6_1[@]}")
+
+for url in "${urls[@]}"
+do
+    wget $url -P /tmp/kernel
+done
 
 sudo dpkg -i /tmp/kernel/*.deb
 sudo update-grub
