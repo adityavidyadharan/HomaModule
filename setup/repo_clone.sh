37111b53eb79c2d6db63b7c5fd3322b38f3e4c9a
adityavidyadharan <aditya@vidyadharan.com>
2024-02-27 10:25:27 -0500

    fix clone script

--- setup/repo_clone.sh
+++ setup/repo_clone.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # Prereq: Copy the DNS key to the .ssh folder but renamed as id_ed25519 (remove the _DNS)
 
 # Usage: ./repo_clone.sh <number of nodes> <mode>
@@ -16,22 +16,27 @@ sudo apt-get install parallel -y
 if [ $mode -eq 0 ]
 then
     
-    seq 1 $end | parallel -j 0 '
+    seq 1 $end | parallel --lb -j 0 '
+        i={}
         echo "Cloning repo on node $i"
-        ssh node$i "git clone https://github.com/adityavidyadharan/HomaModule.git"
-        ssh node$i "cd HomaModule; git switch DNS"
-        ssh node$i "cd HomaModule; git pull"
+        ssh -o StrictHostKeyChecking=no node$i "git clone https://github.com/adityavidyadharan/HomaModule.git"
+        echo "Cloned repo on ndoe $i"
+        ssh -o StrictHostKeyChecking=no node$i "cd HomaModule; git switch DNS"
+        ssh -o StrictHostKeyChecking=no node$i "cd HomaModule; git pull"
+        echo "On DNS branch on node $i"
 
         # Start kernel patching
-        ssh node$i "cd HomaModule/setup; ./kernel_patch.sh"
+        ssh -o StrictHostKeyChecking=no node$i "cd HomaModule/setup; ./kernel_patch.sh"
     '
 
-endif
+fi
 
 if [ $mode -eq 1 ]
 then
-    seq 1 $end | parallel -j 0 '
+    seq 1 $end | parallel --lb -j 0 '
+        i={}
         echo "Executing further_setup.sh on node $i"
-        ssh node$i "cd HomaModule/setup; ./further_setup.sh"
+        ssh -o StrictHostKeyChecking=no node$i "cd HomaModule; ./setup/further_setup.sh"
     '
-endif
+fi
+
